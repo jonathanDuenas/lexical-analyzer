51f7954e23748f299d68cac2cc3999682570acfc
Luis De Paz <lpaz@applaudostudios.com>
2018-09-26 21:49:01 -0600

    Cambiar cadenas a comillas simples para probar strings

--- in.py
+++ in.py
@@ -1,8 +1,9 @@
 x = 1
 y = x + 1
 if x == 1:
-    # indented four spaces
-    print("x is 1.")
+	# indented four spaces
+	print('x is 1.')
+	print('lol')
 
 for i in range(10):
-    print(i)
+	print(i)
